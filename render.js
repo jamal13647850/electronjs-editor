b718135761bc710524248509a891544da86ec540
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2018-07-06 13:35:09 +0430

    Some changes

---
+++ render.js
@@ -0,0 +1,84 @@
+let newButton , openButton , saveButton;
+let editor;
+let menu;
+let fileEntry;
+let hasWriteAccess;
+
+const { remote} = require('electron');
+const { Menu , dialog} = remote;
+const fs = require('fs');
+
+const handleNewButton=()=>{
+    console.log('new');
+};
+const handleOpenButton=()=>{
+
+};
+const handleSaveButton=()=>{
+
+};
+
+const initContextMenu = ()=>{
+    menu = Menu.buildFromTemplate([
+        {
+            label : "File",
+            submenu : [
+                {
+                    label : "New File",
+                    accelerator : 'CmdOrCtrl+N',
+                    click(){handleNewButton()}
+                },
+                {
+                   role:'quit'
+                }
+            ]
+        },
+        {
+            label : "Edit",
+            submenu : [
+                {
+                    role:'copy'
+                },
+                {
+                    role:'cut'
+                },
+                {
+                    role:'paste'
+                }
+            ]
+        },
+        {
+            label : "View",
+            submenu : [
+                {
+                    role:'reload'
+                },
+                {
+                    role:'toggledevtools'
+                }
+            ]
+        }
+    ]);
+    Menu.setApplicationMenu(menu);
+};
+
+document.addEventListener('DOMContentLoaded',()=>{
+    initContextMenu();
+
+    newButton = document.getElementById('new');
+    openButton = document.getElementById('open');
+    saveButton = document.getElementById('save');
+
+    newButton.addEventListener('click',handleNewButton);
+    openButton.addEventListener('click',handleOpenButton);
+    saveButton.addEventListener('click',handleSaveButton);
+
+    editor = CodeMirror(
+        document.getElementById('editor'),
+        {
+            mode :{ name : 'javascript' , json : true},
+            lineNumbers: true,
+            theme : 'lesser-dark'
+        }
+    )
+});
